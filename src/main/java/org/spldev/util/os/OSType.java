49f4c6b56a3aaaf58acef05a2244fba0e0f59f65
Elias Kuiter <info@elias-kuiter.de>
2021-09-27 16:54:14 +0200

    Fixed formatting

--- src/main/java/org/spldev/util/os/OSType.java
+++ src/main/java/org/spldev/util/os/OSType.java
@@ -1,11 +1,12 @@
 package org.spldev.util.os;
 
 public class OSType {
-    
-    private static String OS = System.getProperty("os.name").toLowerCase();
-    public static boolean IS_WINDOWS = (OS.indexOf("win") >= 0);
-    public static boolean IS_MAC = (OS.indexOf("mac") >= 0);
-    public static boolean IS_UNIX = (OS.indexOf("nix") >= 0 || OS.indexOf("nux") >= 0 || OS.indexOf("aix") > 0);;
 
-    private OSType(){}
+	private static String OS = System.getProperty("os.name").toLowerCase();
+	public static boolean IS_WINDOWS = (OS.indexOf("win") >= 0);
+	public static boolean IS_MAC = (OS.indexOf("mac") >= 0);
+	public static boolean IS_UNIX = (OS.indexOf("nix") >= 0 || OS.indexOf("nux") >= 0 || OS.indexOf("aix") > 0);;
+
+	private OSType() {
+	}
 }
