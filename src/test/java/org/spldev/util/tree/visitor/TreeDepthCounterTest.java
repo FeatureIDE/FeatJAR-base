17b63cc1fab3ba6b2000d9e82b97ad9b746494a7
Elias Kuiter <info@elias-kuiter.de>
2022-07-11 13:39:02 +0200

    Updated license headers

--- src/test/java/org/spldev/util/tree/visitor/TreeDepthCounterTest.java
+++ src/test/java/org/spldev/util/tree/visitor/TreeDepthCounterTest.java
@@ -1,23 +1,23 @@
 /* -----------------------------------------------------------------------------
- * Util Lib - Miscellaneous utility functions.
- * Copyright (C) 2021-2022  Sebastian Krieter
+ * util - Common utilities and data structures
+ * Copyright (C) 2020 Sebastian Krieter
  * 
- * This file is part of Util Lib.
+ * This file is part of util.
  * 
- * Util Lib is free software: you can redistribute it and/or modify it
+ * util is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License,
+ * the Free Software Foundation, either version 3.0 of the License,
  * or (at your option) any later version.
  * 
- * Util Lib is distributed in the hope that it will be useful,
+ * util is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
  * 
  * You should have received a copy of the GNU Lesser General Public License
- * along with Util Lib.  If not, see <https://www.gnu.org/licenses/>.
+ * along with util. If not, see <https://www.gnu.org/licenses/>.
  * 
- * See <https://github.com/skrieter/utils> for further information.
+ * See <https://github.com/FeatJAR/util> for further information.
  * -----------------------------------------------------------------------------
  */
 package org.spldev.util.tree.visitor;
