3354c881476bbf14f521b72b9ca2829022feee22
Elias Kuiter <info@elias-kuiter.de>
2023-01-12 10:30:35 +0100

    Revised sharpSAT

--- src/test/java/de/featjar/base/data/IComputationTest.java
+++ src/test/java/de/featjar/base/data/IComputationTest.java
@@ -35,14 +35,14 @@ class IComputationTest {
     void simpleComputation() {
         {
             IComputation<Integer> computation = Computations.of(42);
-            assertEquals(42, computation.getResult().get());
+            assertEquals(42, computation.get().get());
             assertFalse(FeatJAR.cache().has(computation));
         }
         assertTrue(FeatJAR.cache().getCachedComputations().isEmpty());
 
         FeatJAR.run(fj -> {
             IComputation<Integer> computation = Computations.of(42);
-            assertEquals(42, computation.getResult().get());
+            assertEquals(42, computation.get().get());
             assertTrue(FeatJAR.cache().has(computation));
             assertFalse(FeatJAR.cache().getCachedComputations().isEmpty());
         });
@@ -50,7 +50,7 @@ class IComputationTest {
         assertTrue(FeatJAR.cache().getCachedComputations().isEmpty());
         {
             IComputation<Integer> computation = Computations.of(42);
-            assertEquals(42, computation.getResult().get());
+            assertEquals(42, computation.get().get());
             assertFalse(FeatJAR.cache().has(computation));
         }
         assertTrue(FeatJAR.cache().getCachedComputations().isEmpty());
@@ -84,9 +84,9 @@ class IComputationTest {
     void chainedComputation() {
         IComputation<Integer> computation = Computations.of(42);
         IComputation<Boolean> isEvenComputation = new ComputeIsEven(async(42));
-        assertTrue(isEvenComputation.getResult().get());
-        assertTrue(computation.map(ComputeIsEven::new).getResult().get());
-        assertTrue(computation.map(ComputeIsEven::new).getResult().get());
+        assertTrue(isEvenComputation.get().get());
+        assertTrue(computation.map(ComputeIsEven::new).get().get());
+        assertTrue(computation.map(ComputeIsEven::new).get().get());
     }
 
     static class ComputeIsParity extends AComputation<Boolean> implements IInputDependency<Integer> {
@@ -127,22 +127,20 @@ class IComputationTest {
     void computationWithArguments() {
         IComputation<Integer> computation = Computations.of(42);
         assertTrue(new ComputeIsParity(computation, async(ComputeIsParity.Parity.EVEN))
-                .getResult()
+                .get()
                 .get());
         assertFalse(new ComputeIsParity(computation, async(ComputeIsParity.Parity.ODD))
-                .getResult()
+                .get()
                 .get());
         assertTrue(computation
                 .map(c -> new ComputeIsParity(c, async(ComputeIsParity.Parity.EVEN)))
-                .getResult()
+                .get()
                 .get());
     }
 
     @Test
     void allOfSimple() {
-        Pair<Integer, Integer> r = Computations.of(Computations.of(1), Computations.of(2))
-                .getResult()
-                .get();
+        Pair<Integer, Integer> r = Computations.of(Computations.of(1), Computations.of(2)).get().get();
         assertEquals(1, r.getKey());
         assertEquals(2, r.getValue());
     }
@@ -151,7 +149,7 @@ class IComputationTest {
     void allOfComplex() {
         IComputation<Integer> c1 = Computations.of(42);
         IComputation<Boolean> c2 = c1.map(ComputeIsEven::new);
-        Pair<Integer, Boolean> r = Computations.of(c1, c2).getResult().get();
+        Pair<Integer, Boolean> r = Computations.of(c1, c2).get().get();
         assertEquals(42, r.getKey());
         assertEquals(true, r.getValue());
     }
@@ -175,7 +173,7 @@ class IComputationTest {
             }
         };
         IComputation<Boolean> c2 = c1.map(ComputeIsEven::new);
-        Pair<Integer, Boolean> r = Computations.of(c1, c2).getResult().get();
+        Pair<Integer, Boolean> r = Computations.of(c1, c2).get().get();
         assertEquals(42, r.getKey());
         assertEquals(true, r.getValue());
     }
